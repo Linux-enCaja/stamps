9d06970cfd1496ca50af731b0976775a404155a3
Carlos Camargo <cicamargoba@gmail.com>
2011-07-30 12:32:54 -0500

    Se removieron ejemplos redundantes

--- Examples/propendous-imx233_board/bootstream-1.0/chumby_boot/include/fb.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FB_H__
-#define __FB_H__
-
-
-#define FRAMEBUFFER_BYTES (320*240*2)
-
-
-
-#endif //__FB_H__
--- Examples/propendous-imx233_board/bootstream-1.0/chumby_boot/include/ts.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __TS_H__
-#define __TS_H__
-
-void ts_init();
-int ts_pressed();
-
-
-#endif //__TS_H__
